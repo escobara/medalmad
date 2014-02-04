6d33721c48fbb4700ee6fefad0414e3058abe414
David Mills <david@unisonagency.com>
2014-02-04 11:15:17 -0500

    login updates

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,7 +1,7 @@
 class LeaguesController < ApplicationController
 	before_filter :authenticate_user!
 
-	respond_to :json, :html
+	respond_to :js, :json, :html
 
   def index
     @leagues = current_user.leagues.all
@@ -19,11 +19,11 @@ class LeaguesController < ApplicationController
 
   def create
     @league = current_user.leagues.create(league_params)
-    @league.photo = params[:IMAGEDATA] if params[:IMAGEDATA].present? 
     @league.commissioner_id = current_user.id 
     respond_with(@league) do |format|
       if @league.save
         flash[:notice] = 'league was successfully created.'
+        format.js {render @league}
         format.html { redirect_to(@leagues) }
       else
         format.html { render :action => "new" }
@@ -42,7 +42,7 @@ class LeaguesController < ApplicationController
   private
 
   def league_params
-    params.require(:league).permit(:name, :photo, :private, :commissioner_id)   
+    params.require(:league).permit(:name, :photo, :is_private)   
   end
 
 end
--- app/models/league.rb
+++ app/models/league.rb
@@ -3,8 +3,6 @@ class League < ActiveRecord::Base
 	has_many :league_memberships
   has_many :users, through: :league_memberships
 
-	has_attached_file :photo, :styles => {  :small => "150x150>"}, :default_url => "/images/:style/missing.png"
-
 	validates :commissioner_id, presence: true
 	belongs_to :user, foreign_key: "commissioner_id"
 
